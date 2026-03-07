c9b5d4965e73245bdfd10941aa801fa8c0900f38
unknown-nymus18 <felixasante2005@gmail.com>
2026-03-07 14:07:38 +0000

    solved work

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -11,6 +11,19 @@ import User from "../models/User.js";
 
 const authMiddleware = async (req, res, next) => {
   //  implement here
+  try{
+    const token = req.header("Authorization")?.replace("Bearer ","");
+    if(!token){
+      return res.status(401).json({message:"No token, authorization denied"});
+    } 
+
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  }
+  catch(err){
+    res.status(401).json({message:"Token is not valid"})
+  }
 };
 
 export default authMiddleware;
